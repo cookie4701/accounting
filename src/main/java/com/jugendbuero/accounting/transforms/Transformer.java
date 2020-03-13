8c9046ce507d27e1d0562fd39d9773637351df33
Pascal Kuck <pascal@pkit.eu>
2020-03-13 15:53:30 +0100

    Added static member function to transform a BankAccountEntity into a BankAccountIdDto

--- src/main/java/com/jugendbuero/accounting/transforms/Transformer.java
+++ src/main/java/com/jugendbuero/accounting/transforms/Transformer.java
@@ -2,6 +2,7 @@ package com.jugendbuero.accounting.transforms;
 
 import com.jugendbuero.accounting.dto.AccountingPlanDto;
 import com.jugendbuero.accounting.dto.BankAccountDto;
+import com.jugendbuero.accounting.dto.BankAccountIdDto;
 import com.jugendbuero.accounting.entities.AccountingPlanEntity;
 import com.jugendbuero.accounting.entities.BankAccountEntity;
 
@@ -28,6 +29,16 @@ public class Transformer {
 		dto.setLabel(ent.getLabel());
 		dto.setDateLastReset(ent.getDateLastReset());
 		
+		return dto;
+	}
+	
+	public static BankAccountIdDto getBankAccountIdDto(BankAccountEntity ent) {
+		BankAccountIdDto dto = new BankAccountIdDto();
+		dto.setAmountLastReset(ent.getAmountLastReset());
+		dto.setCode(ent.getCode());
+		dto.setLabel(ent.getLabel());
+		dto.setDateLastReset(ent.getDateLastReset());
+		dto.setId(ent.getId());
 		return dto;
 	}
 
